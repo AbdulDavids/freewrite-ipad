dc990925fac23b6f4aae5bb6f78fbcf9518df1da
Abdul Baari Davids <abdulbaaridavids04@gmail.com>
2025-06-12 19:58:06 +0200

    Initial Commit

---
+++ freewrite-ipad/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  freewrite-ipad
+//
+//  Created by Abdul Baari Davids on 2025/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ freewrite-ipad/freewrite_ipadApp.swift
@@ -0,0 +1,17 @@
+//
+//  freewrite_ipadApp.swift
+//  freewrite-ipad
+//
+//  Created by Abdul Baari Davids on 2025/06/12.
+//
+
+import SwiftUI
+
+@main
+struct freewrite_ipadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
